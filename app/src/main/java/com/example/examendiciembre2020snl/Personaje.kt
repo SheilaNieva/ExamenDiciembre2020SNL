29cd083a656ee5dd7ebf00f8c8c73a264a800c25
SheilaNieva <sheilanievalo.2000@gmail.com>
2020-12-11 12:21:18 +0100

    Clase Personaje creada
    clase pollo creada

---
+++ app/src/main/java/com/example/examendiciembre2020snl/Personaje.kt
@@ -0,0 +1,14 @@
+package com.example.examendiciembre2020snl
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Personaje (@PrimaryKey var id: String = "",
+                      var nombre: String="",
+                      var raza : String="",
+                      var ki : Int =0,
+                      var ataque: String="",
+                      var imagen: String=""): RealmObject(){
+    constructor(nombre:String, raza:String, ki:Int, ataque:String, imagen:String):this(UUID.randomUUID().toString(), nombre, raza, ki, ataque, imagen)
+}
