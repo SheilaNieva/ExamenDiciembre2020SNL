806251e20c101c3401a39d41351f22a6ab099252
SheilaNieva <sheilanievalo.2000@gmail.com>
2020-12-11 12:24:35 +0100

    Consultas basicas
    En la clase Controller hemos creado las consultas basicas que se suelen utilizar en Realm para la clase Personaje

--- app/src/main/java/com/example/examendiciembre2020snl/PersonajeController.kt
+++ app/src/main/java/com/example/examendiciembre2020snl/PersonajeController.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import io.realm.kotlin.where
 
 object PersonajeController {
     private const val DATOS_BD = "REPASO_EXAMEN"
@@ -19,4 +20,42 @@ object PersonajeController {
         Realm.setDefaultConfiguration(config)
         Log.d("Datos", "Iniciando Realm")
     }
+
+    //Consultas basicas en una base de datos Realm según nuestra clase pollo
+
+
+    //Insertamos un nuevo objeto en nuestra bbdd
+    fun insertDato(dato: Personaje) {
+        Realm.getDefaultInstance().executeTransaction {
+            it.copyToRealm(dato); // Copia, inserta
+        }
+    }
+
+    //Actualizamos un personaje en nuestra base de datos
+    fun updateDato(dato: Personaje) {
+        Realm.getDefaultInstance().executeTransaction {
+            it.copyToRealmOrUpdate(dato)
+        }
+    }
+
+    //Obtenemos toda la lista de personajes que tengamos guardados en la bbdd
+    fun selectAll(): MutableList<Personaje>? {
+        return Realm.getDefaultInstance().copyFromRealm(
+            Realm.getDefaultInstance().where<Personaje>().findAll()
+        )
+    }
+
+    //Borramos toda la lista que exista en la table de Personajes de la bbdd
+    fun removeAll() {
+        Realm.getDefaultInstance().executeTransaction {
+            it.deleteAll();
+        }
+    }
+
+    //Consultamos por ID
+    fun consultaPorID(id: String): Personaje? {
+        return Realm.getDefaultInstance().where<Personaje>().equalTo("id", id).findFirst()
+    }
+
+
 }
